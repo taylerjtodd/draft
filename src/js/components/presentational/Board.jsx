4c5d31218320a9670ac9ab502ded84ff1fd63d88
Tayler Todd <taylerjtodd@gmail.com>
2019-08-11 22:08:55 -0500

    Dataload

--- src/js/components/presentational/Board.jsx
+++ src/js/components/presentational/Board.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import DataLoad from "../../DataLoad.js"
 
 class Board extends Component {
-  constructor() {
+  constructor({ state, handleChange }) {
     super();
-    this.state = {
-      currentTab: window.location.hash
-    };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = state;
+    this.handleChange = handleChange;
+
+    if (!state.players) {
+      this.handleChange({type: 'SET_PLAYERS', players: this.initPlayers()});
+    }
   }
 
-  handleChange(event) {
-    console.log(event)
+  initPlayers() {
+    let players = DataLoad.projections();
+    let rankings = DataLoad.ranks();
+
+    rankings.forEach(player => {
+        var pl = players[player.pos].find(p => p.name == player.name);
+        if(pl) {
+          Object.assign(pl, player);
+        }
+    });
+    return players;
   }
 
   render() {
@@ -21,6 +33,7 @@ class Board extends Component {
         type='text'
         className="form-control"
         value='Board'
+        onChange={() => this.handleChange({ type: 'BOARD_CHANGE' })}
       />
     );
   }
