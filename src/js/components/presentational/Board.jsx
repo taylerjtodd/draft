5e6e1084e467e9c7e7654457c0cda3b27a442c36
Tayler Todd <taylerjtodd@gmail.com>
2019-08-11 22:20:08 -0500

    VBD sort

--- src/js/components/presentational/Board.jsx
+++ src/js/components/presentational/Board.jsx
@@ -9,7 +9,7 @@ class Board extends Component {
     this.handleChange = handleChange;
 
     if (!state.players) {
-      this.handleChange({type: 'SET_PLAYERS', players: this.initPlayers()});
+      this.handleChange({ type: 'SET_PLAYERS', players: this.initPlayers() });
     }
   }
 
@@ -18,12 +18,57 @@ class Board extends Component {
     let rankings = DataLoad.ranks();
 
     rankings.forEach(player => {
-        var pl = players[player.pos].find(p => p.name == player.name);
-        if(pl) {
-          Object.assign(pl, player);
-        }
+      var pl = players[player.pos].find(p => p.name == player.name);
+      if (pl) {
+        Object.assign(pl, player);
+      }
     });
-    return players;
+
+    var aveQB = players.qb[11].ppg;
+    var aveRB = players.rb[40].ppg;
+    var aveWR = players.wr[44].ppg;
+    var aveTE = players.te[8].ppg;
+    var aveK = players.k[1].ppg;
+    var aveDST = players.dst[2].ppg;
+
+    function pad(num, size) {
+      var s = num + "";
+      while (s.length < size) s = "0" + s;
+      return s;
+    }
+
+    var insertPointDif = function (players, baseline) {
+      players.forEach(function (player) {
+        player.pointDif = player.ppg - baseline;
+      });
+      players.sort(function (a, b) {
+        return b.pointDif - a.pointDif;
+      });
+      players.forEach(function (player, i) {
+        player.posrank = pad(i + 1, 2);
+      });
+    };
+
+    insertPointDif(players.qb, aveQB);
+    insertPointDif(players.rb, aveRB);
+    insertPointDif(players.wr, aveWR);
+    insertPointDif(players.te, aveTE);
+    insertPointDif(players.k, aveK);
+    insertPointDif(players.dst, aveDST);
+
+    let allPlayers = players.qb.concat(players.rb).concat(players.wr).concat(players.te).concat(players.k).concat(players.dst);
+
+    allPlayers.sort(function (a, b) {
+      return b.pointDif - a.pointDif;
+    });
+
+    allPlayers.forEach(function (player, i) {
+      player.vrank = i + 1;
+      player.rank = i + 1;
+      player.displayPosition = player.position + player.posrank;
+    });
+
+    return allPlayers;
   }
 
   render() {
